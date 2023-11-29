5dfee306c1b2b6c48d007ba15a730f5744b7eaad
nanangsuharyanto <151450401+nanangsuharyanto@users.noreply.github.com>
2023-11-29 10:42:50 +0800

    Add files via upload

---
+++ genap ganjil.py
@@ -0,0 +1,17 @@
+#input bilangan
+bilangan = int(input("Masukkan bilangan: "))
+
+#menentukan ganjil genap
+def cek_bilangan(bilangan):
+    if bilangan % 2 == 0:
+        print(f"{bilangan} adalah bilangan genap")
+    else:
+        print(f"{bilangan} adalah bilangan ganjil")
+    return bilangan
+
+#mengalikan bilangan setelahnya
+bilangan_setelahnya = bilangan+1
+hasil = bilangan*bilangan_setelahnya
+
+#menampilkan hasil
+print(f"Hasil perkalian dari {bilangan} dengan bilangan setelahnya ({bilangan_setelahnya}) adalah {hasil}")
